32f8da0bb4d082165466011c0aa79fe2d3ab2084
Jimmy Hough <jimmyhoughjr@me.com>
2022-12-13 15:17:19 -0600

    Created swiftui project

---
+++ SitAround/SitAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SitAroundApp.swift
+//  SitAround
+//
+//  Created by Jimmy Hough Jr on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SitAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
